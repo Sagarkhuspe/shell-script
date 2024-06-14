2a2179f921feec0194196130bff75b038bc95822
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-06-14 08:38:40 +0530

    bash auto code

--- 002-escSequencecharcters.sh
+++ 002-escSequencecharcters.sh
@@ -9,6 +9,7 @@ echo line1
 echo line2
 
 echo -e "line3\nline4"
+echo -e "line5\n\tline6"
 
 # Whenever you're using special characters , always enclose them in DOUBLE QUOTES
 # " "  : Double Quotes 
