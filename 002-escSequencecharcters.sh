84b02219d739880078e02dfe27c17bae4101f6f4
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-06-14 08:31:06 +0530

    bash auto code

---
+++ 002-escSequencecharcters.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#escape seqence character
+
+#\n  : new line
+#\t  : move to the cursor next tab space
+
+echo line1
+echo line2
+
+echho -e "line3\nline4"
+
+#-e when you are using special character in echo command and always use double coloun.
