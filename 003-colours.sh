2749df32625efbf5b1c751e97acb94ff292cf493
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-06-14 08:57:32 +0530

    bash auto code

--- 003-colours.sh
+++ 003-colours.sh
@@ -16,3 +16,22 @@
 # \e...\e[0m used for start and exit command.
 
 echo -e "\e[33m i am printing colour \e[0m"
+
+cho -e "\e[32m I am printing GREEN COLOR \e[0m"
+echo -e "\e[31m I am printing RED COLOR \e[0m"
+echo -e "\e[33m I am printing Yellow COLOR \e[0m"
+echo -e "\e[34m I am printing Blue COLOR \e[0m"
+echo -e "\e[35m I am printing Magenta COLOR \e[0m"
+
+# () : Paranthesis
+# [] : Square Bracker
+# {} : Flower Bracker
+
+### How can we add a baccGround color ? Here is the syntax 
+
+# echo -e "\e[43;31m I am printing Text with a backGround Color \e[0m"
+
+echo -e "\n\n"
+
+echo -e "\e[43;31m I am printing Text with a backGround Color \e[0m"
+
