ea7923e5c61528284e006a1eb53bcdf146d564a9
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-06-15 11:19:36 +0530

    bash auto code

---
+++ 006-specialVariables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#special varibles are pre-defined varibles with specific special pre-defined power.
+
+#special varibles are $0 to $9, $* , $@, $#, $$
+
+# can you create on special varibles on bash, 100% NO..
+# you can just use special variables that are offered.
+
+a=3000
+
