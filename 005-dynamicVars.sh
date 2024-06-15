e20ca3e7799d2942e4df9a8a9deb381a742f8782
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-06-15 10:18:37 +0530

    bash auto code

--- 005-dynamicVars.sh
+++ 005-dynamicVars.sh
@@ -4,7 +4,7 @@
 # NO_OF_SESSIONS="4"       this is called as hardCoding and hardCoding is a bad way and whereever possible you should fetch the values dynamically.
 
 
-DATE=$(date +%F)
+DATE=date +%F
 NO_OF_SESSIONS="$(who|wc -l)"
 
 echo -e "Todays date is ${DATE}" 
