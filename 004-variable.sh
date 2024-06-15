9c29b2114458d2c8ab2b0c8e5105e3a9a31b2e8e
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-06-15 09:34:32 +0530

    bash auto code

---
+++ 004-variable.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# What is a variable ?
+# A variable is something that holds data and can change dynamically
+
+# the advantage & disadvantage in shell is that there are no Data Types
+
+# Declaring the variables
+
+a=10
+b=def
+
+# In bash, everything will be considered as string by default.
+
+# How can we print a variable ?  $var
+
+# $ is a special variableand this helps you in printing the value of the variable 
+
+echo "$a" 
+echo " the value of variable b is $b"
+echo " the value of variable b is ${b}"
+
+#echo "$a" or  echo {$a} we can use both. if one is not working then user another cammand.
