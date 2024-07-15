1dfad2d2c6d7b822ce3d4567640462668b5c9537
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-15 09:41:55 +0530

    bash auto code

--- 006-specialVariables.sh
+++ 006-specialVariables.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
+#!/bin/bash 
 
-#special varibles are pre-defined varibles with specific special pre-defined power.
+# Special Variables are the pre-defined variables with specific special pre-defined powers.
 
-#special varibles are $0 to $9, $* , $@, $#, $$
+# Special Variables are $0 to $9, $* , $@, $# ,  $$
 
-# can you create on special varibles on bash, 100% NO..
-# you can just use special variables that are offered.
+# Can you create special variables on Bash ? 100% No , you can just use the special variables that are offered.
 
-a=3000
+a=3000 
 TEAM=$1
 PROJECT=$2
 echo "Value of a is $a" 
@@ -16,5 +15,19 @@ echo "Script Name is : $0"
 # $0 : Prints the name of the script you're running
 
 echo -e "Name of the team running the script is : \e[33m $TEAM \e[0m"
-echo -e "Name of this project is : \e[33m $project \e[0m"
+echo -e "Name of the project is : \e[33m $PROJECT \e[0m"
+
+### bash scriptName.sh    arg1   arg2   arg3   arg4 . . . . . . arg9
+### bash scriptName.sh  firstArg   secondArg  thirdArd 
+### bash abc.sh  10   20   30 
+# bash arg1  arg2  arg3 arg4  arg5  arg6 arg7  arg8  arg9  arg10 
+#        1     2     3    4    5     6    7      8    9      10
+# ( You supply up to 9 arguments)
+
+
+echo $0   # prints the scriptName
+echo $#   # Prints the arguments used in the script 
+echo $?   # Prints the exit code of the previous command 
+echo $*   # Prints all the arguments user
+echo $@   # Prints all the arguments user
 
