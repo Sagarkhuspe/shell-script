0c69997761830304514cbe29ceaa61eda08df9b1
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-21 14:05:35 +0530

    bash auto code

---
+++ 008-quotes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#  "" ---> Double quotes
+#  '' ----> Single quotes--> single quotes always eliminates(kill) the power the special varibales.
+
+a=100
+echo $?
+echo '$?'
+echo "value of a is"
+echo 'value of a is'
