9a4da34bbd793e6796c0cf0cbc169c82081120ce
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-23 09:03:51 +0530

    bash auto code

--- 010-functions.sh
+++ 010-functions.sh
@@ -2,10 +2,10 @@
 
 #There are four tyope of commands available
 
-1)Binary                      -->(Used for /bin , /sbin)
-2)Aliases                     -->(Used for shortcut ex. alias gp="git pull")
-3)Shell built-in commands     -->(cd, pwd, exit, export, alias)
-4)Functions              -->(you have common pattern and this pattern using function can effictively)
+#1)Binary                      -->(Used for /bin , /sbin)
+#2)Aliases                     -->(Used for shortcut ex. alias gp="git pull")
+#3)Shell built-in commands     -->(cd, pwd, exit, export, alias)
+#4)Functions              -->(you have common pattern and this pattern using function can effictively)
 
 #how to declare function
 
