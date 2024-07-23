c26dc62d4fb4ea8bbe0096f6bb89a3acd5359abb
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-23 09:41:21 +0530

    bash auto code

---
+++ 011-comments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#this is single line comments.
+
+#here is using multiple line comments.
+
+echo printing multiline comments
+<<comments
+a=10
+b=20
+c=30
+echo "value of a is $a"
+comments
+echo "value of bi= is $b"
