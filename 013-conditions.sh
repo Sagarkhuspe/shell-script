93ba0f7c65cee3141876ed3bdd3e8b1867d9a296
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-24 11:27:49 +0530

    bash auto code

--- 013-conditions.sh
+++ 013-conditions.sh
@@ -15,12 +15,16 @@ Action=$1
 
 case $Action in
       start)
-      echo "starting the dispatch service" ;;
+      echo -e "\e[32m starting the dispatch service  \e[0m" ;;
 
       stop)
-      echo "stopping the dispatch service" ;;
+      echo -e "\e[33m stopping the dispatch service  \e[0m" ;;
      
       restart)
-      echo "restarting the dispatch service" ;;
+      echo -e "\e[34m restarting the dispatch service  \e[0m" ;;
+
+       *)
+        echo -e "\e[35m Valid options are: start  stop  restart   only \e[0m"
+        echo -e "\e[31m Usage: \e[0m \n\tbash conditions.sh  start  \e[0m"
 
 esac      
