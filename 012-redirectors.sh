b3d678f32793b49bb8aedba0e9e5ae7f8fd91b53
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-24 10:28:21 +0530

    bash auto code

--- 012-redirectors.sh
+++ 012-redirectors.sh
@@ -2,7 +2,13 @@
 
 #how to take inpur, how to give output, how to handle errors.
 
-     1) input redirectors (<) : we use this input fron file..ex $mysql < /student/sql
+     1) input redirectors (<)      : we use this input fron file..ex $mysql < /student/sql
         meaning of that mysql command will be executed by reading the file of /student/sql.
 
-        
+     2)outpur redirectos (> or >>) :  ps >> -ls ltr... ps send the output to ls ltr.
+
+     # Outputs 
+        1) Standard Output                          1> or > or >> ( Append )
+        2) Standard Error                           2> or 2>> 
+        3) Standard Output and Standard Error       &> or &>>   
+
