d1d94cf40c262e5860497302f8a40707480eb052
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-25 12:01:28 +0530

    bash auto code

---
+++ 016-loops.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#lopps is used for when you want to run something n times contineously , then we use.
+
+# there are two type of loops
+
+#  1) for loops --> when you know how many time to executed.
+#  2)while loops--> when you dont know how many tine exrcuted. we use this and whilw only if condition is treu.
+
+#ex
+
+# echo value is 10
+# echo value is 20
+# echo value is 30
+# echo value is 40
+
+# syntax
+#  for x in a b c d ; do
+#  echo -e "value od x is $x"
+#  done
+
+for x in 10 20 30 40 ; do
+   echo "value of is $x /n/t"
+done
