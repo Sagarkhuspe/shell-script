09e4443cc26a32cc8ae4af45683f6e72dba2ea39
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-25 11:36:24 +0530

    bash auto code

--- 015-ifelse.sh
+++ 015-ifelse.sh
@@ -4,15 +4,15 @@ echo -e "demo only for if, if else, else if condition usage"
 
 Action=$1
 
-if ["$Action" = "start"] ; then
+if [ "$Action" = "start" ] ; then
 echo -e "\e[32m starting dispatch service only \e[0m"
 exit 0
 
-elif ["$Action" = "stop"] ; then
+elif [ "$Action" = "stop"]  ; then
 echo -e "\e[33m stopping dispatch service only \e[0m"
 exit 1
 
-elif ["$Action" = "restart"] ; then
+elif [ "$Action" = "restart"]  ; then
 echo -e "\e[34m restarting dispatch service only \e[0m"
 exit 2
 
