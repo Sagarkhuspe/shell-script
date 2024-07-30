72b2a5ad44459cdcbc2db90e11d1c1a151037d74
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-30 12:06:44 +0530

    bash auto code

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -2,6 +2,8 @@
 
 # if we running the script if sudo or root user or not , if ot exit tht script.
 # if you need sudo then use another method the run the script. uid of root user always 0 
+# i did want see output logs so we can use output redirectory(&>>).
+
 
 ID=$(id -u)
 
@@ -12,10 +14,28 @@ fi
 
 
 echo "installing nginix web server"
-dnf install nginx -y
+dnf install nginx -y               &>> /tmp/fronend.log
+if [$? eq 0 ] ; then
+     echo -n "\e[32m success \e[0m"
+else 
+  echo -n "\e[32m failure \e[0m"
+exit 2
+fi
 
 echo "enable the service"
-systemctl enable nginx
+systemctl enable nginx              &>> /tmp/fronend.log
+if [$? eq 0 ] ; then
+     echo -n "\e[32m success \e[0m"
+else 
+  echo -n "\e[32m failure \e[0m"
+exit 2
+fi
 
 echo "starting web server"
-systemctl enable nginx
+systemctl enable nginx              &>> /tmp/fronend.log
+if [$? eq 0 ] ; then
+     echo -n "\e[32m success \e[0m"
+else 
+  echo -n "\e[32m failure \e[0m"
+exit 2
+fi
