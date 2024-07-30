9a8b117bc6b811cff33425be86b734adc6ecbdcb
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-30 12:18:50 +0530

    bash auto code

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -3,6 +3,12 @@
 # if we running the script if sudo or root user or not , if ot exit tht script.
 # if you need sudo then use another method the run the script. uid of root user always 0 
 # i did want see output logs so we can use output redirectory(&>>).
+# its not east to always if else use, so we can
+#if [ $? eq 0 ] ; then
+ #    echo -n "\e[32m success \e[0m"
+# else 
+  #echo -n "\e[32m failure \e[0m"
+#exit 2
 
 
 ID=$(id -u)
@@ -14,29 +20,22 @@ fi
 
 LOGFILE="/tmp/fronend.log"
 
-echo "installing nginix web server"
-dnf install nginx -y               &>> $LOGFILE
-if [ $? eq 0 ] ; then
+stat () {
+    if [ $1 eq 0 ] ; then
      echo -n "\e[32m success \e[0m"
 else 
-  echo -n "\e[32m failure \e[0m"
+  echo -n "\e[33m failure \e[0m"
 exit 2
-fi
+}
+
+echo "installing nginix web server"
+dnf install nginx -y               &>> $LOGFILE
+stat $?
 
 echo "enable the service"
 systemctl enable nginx              &>> $LOGFILE
-if [ $? eq 0 ] ; then
-     echo -n "\e[32m success \e[0m"
-else 
-  echo -n "\e[32m failure \e[0m"
-exit 2
-fi
+stat $?
 
 echo "starting web server"
 systemctl enable nginx              &>> $LOGFILE
-if [ $? eq 0 ] ; then
-     echo -n "\e[32m success \e[0m"
-else 
-  echo -n "\e[32m failure \e[0m"
-exit 2
-fi
+stat $?
