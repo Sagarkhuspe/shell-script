3271697c9701335a95f168c0fe7dbc9e8357a8a2
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-30 12:36:28 +0530

    bash auto code

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -17,29 +17,54 @@ if [ $ID -ne 0 ] ; then
     exit 1
 fi 
 
-LOGFILE="/tmp/fronend.log"
+COMPONENT="frontend"
+LOGFILE="/tmp/$1.log"
 
-stat () {
-    if [ $1 -eq 0 ] ; then
-         echo -e "\e[32m success \e[0m"
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m"
     else 
-         echo -e "\e[33m failure \e[0m"
-
-fi
+        echo "\e[31m Failure \e[0m"
+    fi 
 }
-echo -n "installing nginix web server"
-dnf install nginx -y               &>> $LOGFILE
+
+echo -e "\e[35m **************** ______ $COMPONENT Configuration Is Started ______  **************** \e[0m"
+echo -n "Intalling Nginx Web Server :"
+dnf install nginx -y        &>>  $LOGFILE
 stat $?
 
-echo -n "enable the service"
-systemctl enable nginx              &>> $LOGFILE
+echo -n "Enabling the service :"
+systemctl enable nginx      &>>  $LOGFILE
 stat $?
 
-echo -n "starting web server"
-systemctl enable nginx              &>> $LOGFILE
+echo -n "Downloading the $COMPONENT Component: "
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $? 
+
+echo -n "Performing $COMPONENT Cleanup :"
+cd /usr/share/nginx/html 
+rm -rf * &>>  $LOGFILE
+stat $? 
+
+echo -n "Extracting $COMPONENT :"
+unzip /tmp/frontend.zip   &>>  $LOGFILE 
+stat $? 
+
+echo -n "Configuring $COMPONENT :"
+mv ${COMPONENT}-main/* .   &>>  $LOGFILE
+mv static/* .        &>>  $LOGFILE
+rm -rf ${COMPONENT}-main README.md    &>>  $LOGFILE  
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
+echo -n "Updating Reverse Proxy File: "
+    for i in catalogue user cart shipping payment; do 
+        sed -i -e "/$i/s/localhost/$i.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+    done
+stat $?
 
+echo -n "Retarting the Web Server: "
+systemctl restart nginx       &>>  $LOGFILE
+stat $?
 
-
-
+echo -e "\e[35m ****** ______ $COMPONENT Configuration Is Completed ****** ______ \e[0m"
