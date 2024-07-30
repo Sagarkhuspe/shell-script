fd9c9cf1317f86ae5bb8968530de0fd22949f4b2
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-30 10:38:30 +0530

    bash auto code

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -2,4 +2,7 @@
 
 #if yow want to run roboshop after cd you run the script cd roboshop.
 
-bash component/catalogue.sh
+#    bash component/catalogue.sh 
+# we dont want this type hard coding so we can use
+
+bash component/$1
