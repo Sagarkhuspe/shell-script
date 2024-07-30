48dba8ccee9cca286d1099e24947638d14b911e6
Sagarkhuspe <sgrkhuspe@gmail.com>
2024-07-30 11:36:58 +0530

    bash auto code

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # if we running the script if sudo or root user or not , if ot exit tht script.
-# if you need sudo then use another method the run the script.
+# if you need sudo then use another method the run the script. uid of root user always 0 
+
+ID=$(id -u)
+
+if [ $ID ne 0] then
+    echo -e "\e[33m this script expected to run with sudo or root user \e[0m   \n\t ex: bash scriptname compName"
+exit 1
+
+fi
 echo "installing nginix web server"
 dnf install nginx -y
 
